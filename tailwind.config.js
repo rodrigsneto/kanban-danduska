20069f04850a7f4f86aa3205a27c066d32c349ed
Rodrigues Neto <accounts@rodrigsneto.com>
2024-08-04 23:26:15 -0300

    finalizado instalacao e config das bibliotecas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "mainBackgroundColor": '#0D1117',
+        "columnBackgroundColor": '#161C22'
+      }
     },
-    plugins: []
-  }
-  
-  
+  },
+  plugins: []
+}
+
